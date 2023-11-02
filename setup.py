9d945894ed6bb911612f5ce24d9b7607947a46c3
Riccardo Mereu <r.mereu@arduino.cc>
2023-11-02 09:47:51 +0100

    [SETUP] Moves to version 0.1.1

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ classifiers = ['Operating System :: POSIX :: Linux',
                'Topic :: System :: Hardware']
 
 setup(name                          = 'Portenta.GPIO',
-      version                       = '0.1.0',
+      version                       = '0.1.1',
       author                        = 'Riccardo Mereu',
       author_email                  = 'r.mereu@arduino.cc',
       description                   = 'A module to control Portenta GPIO channels',
